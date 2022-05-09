d840f242d0c7324819a296c21a38b062aab9138a
4lexRossi <devalexrossi@gmail.com>
2022-05-08 22:13:45 -0300

    created send email service

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
-
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "eaf99e97af2c52",
+    pass: "53c772b8c0d2e2"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -16,6 +25,17 @@ app.post('/feedbacks', async (req, res) => {
     },
   });
 
+  await transport.sendMail({
+    from: 'Alos Feedget <alos@mailinator.com>',
+    to: "Alex Rossi <alex@mailinator.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+  })
   return res.status(201).json({ data: feedback });
 });
 
